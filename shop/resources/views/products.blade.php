a4f9de0c8a926e26f6925acbb9956c64692ec0fb
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-18 16:56:01 +0300

    added

---
+++ shop/resources/views/products.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+</head>
+<body>
+    <h1>Products</h1>
+    <main>
+        <article>
+            <h2>Doctor Sausage</h2>
+            <img src="images\sosug.jpg" alt="ja"/>
+            <p>sosug very teisti. Buy buy buy sell sell sell</p>
+            <p>1.39 EUR</p>
+        </article>
+    </main>
+</body>
+</html>
